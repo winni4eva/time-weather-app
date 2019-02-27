f6ed8b6518eaf14872c8521d3bde5580af87d6a2
Winnipass <adams4lyf@gmail.com>
2019-02-27 20:30:57 +0000

    feat: Fixed timestamp bug

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const inputs = process.argv.slice(2);
 const weather = require('./lib/weather');
 
 if(inputs.length !== 2) {
-    console.log('Please pass in a location eg Arizona and postal code eg 85001');
+    console.log('Please pass in a location (Arizona) and code eg us');
     return;
 }
 
@@ -16,8 +16,15 @@ const postalCode = inputs[1];
 
 weather.get(location, postalCode, function(status, headers, body){
     if(status === 200) {
-        console.log(body);
-        return;
+        const weatherDescription = body.weather[0].description;
+        var date = new Date(String(body.dt));
+
+        if(date.toDateString() === 'Invalid Date') {
+            date = new Date();
+        }
+        const currentDateTime = date.toDateString();
+        console.log(`The weather is ${weatherDescription} in ${location} for period ${currentDateTime}`);
+    } else {
+        console.log('Failed processing your request, please try again');
     }
-    
 });
