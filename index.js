ec4a23fe9b222fc4d9be5ab33c54fca591aa411b
Winnipass <adams4lyf@gmail.com>
2019-02-27 21:01:13 +0000

    en(style): fixed style errors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 const inputs = process.argv.slice(2);
 const weather = require('./lib/weather');
 
-if(inputs.length !== 2) {
+if (inputs.length !== 2) {
     console.log('Please pass in a location (Arizona) and code eg us');
     return;
 }
@@ -15,11 +15,11 @@ const location = inputs[0];
 const postalCode = inputs[1];
 
 weather.get(location, postalCode, function(status, headers, body){
-    if(status === 200) {
+    if (status === 200) {
         const weatherDescription = body.weather[0].description;
         let date = new Date(String(body.dt));
 
-        if(date.toDateString() === 'Invalid Date') {
+        if (date.toDateString() === 'Invalid Date') {
             date = new Date();
         }
         const currentDateTime = date.toDateString();
